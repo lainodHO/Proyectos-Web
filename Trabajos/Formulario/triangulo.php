bc6ab1c2437e3361de4cb8201ef81a6522ccd868
lainodHO <daniel192412012@gmail.com>
2023-08-30 18:00:18 -0600

    Projects

---
+++ Trabajos/Formulario/triangulo.php
@@ -0,0 +1,21 @@
+<HTML>
+	<HEAD>
+		<TITLE>
+		Resultado	
+		</TITLE>
+	</HEAD>
+	<body bgcolor="#00ff00">
+	<big>Resultado, Area del Triangulo</big><br>
+	<?php
+
+	
+	$base=$_POST["basesita"];
+	$altura=$_POST["alturita"];
+	$area=$base*$altura/2;
+	echo "El area del triangulo es: $area";
+	echo"<br>";
+	echo <HR> SIZE=10 width=330 color=#0000ff align="left>";
+	<?
+	</BODY>
+</BODY>
+</HTML>
